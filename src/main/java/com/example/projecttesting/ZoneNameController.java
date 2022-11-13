e5cdeaf0e5514c143e3471f49f83fd9d14463be2
Jess <jessahillard@gmail.com>
2022-11-12 20:23:43 -0500

    TableView Stuff

--- src/main/java/com/example/projecttesting/ZoneNameController.java
+++ src/main/java/com/example/projecttesting/ZoneNameController.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,6 +18,13 @@ public class ZoneNameController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    private TextField filterField;
+    private TableView<Plant> tableView;
+    private TableColumn<Plant, String> plantName;
+    private TableColumn<Plant, String> zone;
+    private TableColumn<Plant, String> height;
+    private TableColumn<Plant, String> width;
+    private TableColumn<Plant, String> sunlight;
 
 
     public void back(ActionEvent e) throws IOException {
@@ -26,5 +35,7 @@ public class ZoneNameController {
         stage.show();
         System.out.println("You have gone back");
     }
+
+
 
 }
