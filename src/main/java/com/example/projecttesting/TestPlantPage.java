2bb11a59db59bae01c2ca1c9ae3969a77d8bf4d0
Jess <jessahillard@gmail.com>
2022-11-14 20:08:37 -0500

    Added test pages for the plants

---
+++ src/main/java/com/example/projecttesting/TestPlantPage.java
@@ -0,0 +1,16 @@
+package com.example.projecttesting;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class TestPlantPage {
+
+    @FXML
+    Label plantTitle;
+    Label plantHeight;
+    Label plantWidth;
+    Label plantSun;
+
+    public void displayNames()
+
+}
